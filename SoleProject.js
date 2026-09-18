01637ea9dc5e245fd271bd7f00731d9ebf72f4f8
abhiigna-c <abhiigna1077@gmail.com>
2026-09-18 20:56:55 +0530

    login & signup page

--- SoleProject.js
+++ SoleProject.js
@@ -14,5 +14,49 @@ menus.forEach(function(menu) {
             content.classList.remove("active");
         }, 500);
     });
+});
 
+const loginBtn = document.getElementById("login");
+const signupBtn = document.getElementById("signup");
+
+const loginPopup = document.getElementById("loginPopup");
+const signupPopup = document.getElementById("signupPopup");
+
+const closeLogin = document.getElementById("closeLogin");
+const closeSignup = document.getElementById("closeSignup");
+
+const openSignup = document.getElementById("openSignup");
+const openLogin = document.getElementById("openLogin");
+
+loginBtn.addEventListener("click", function(event) {
+  event.preventDefault();
+  signupPopup.style.display = "none";
+  loginPopup.style.display = "flex";
+});
+
+signupBtn.addEventListener("click", function(event) {
+  event.preventDefault();
+  
+  loginPopup.style.display = "none";
+  signupPopup.style.display = "flex";
+});
+
+closeLogin.addEventListener("click", function() {
+  loginPopup.style.display = "none";
+});
+
+closeSignup.addEventListener("click", function() {
+  signupPopup.style.display = "none";
+});
+
+openSignup.addEventListener("click", function(event) {
+  event.preventDefault();
+  loginPopup.style.display = "none";
+  signupPopup.style.display = "flex";
+});
+
+openLogin.addEventListener("click", function(event) {
+  event.preventDefault();
+  signupPopup.style.display = "none";
+  loginPopup.style.display = "flex";
 });
